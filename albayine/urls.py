043830ae00fe52afba6a539c760a4790e63b064c
Abdelbari <77675077+Eniyabla@users.noreply.github.com>
2022-09-07 17:58:13 +0300

    remove library

--- albayine/urls.py
+++ albayine/urls.py
@@ -1,7 +1,5 @@
 from django.conf.urls.static import static
 from django.conf import settings
-from django.contrib import admin
-from django.template.defaulttags import url
 from django.urls import path
 from django.views.static import serve
 
@@ -14,7 +12,7 @@ urlpatterns = [
     path('contact-me',homeViews.contactMe,name='contactme'),
     path('portfolio-detail/<int:id>',homeViews.portfoliodetail,name='portfoliodetail'),
 
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    path(r'^media/(?P<path>.*)$', serve,{'document_root':settings.MEDIA_ROOT}),
+    path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
