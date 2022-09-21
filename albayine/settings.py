a8c01347788d6012eb35edbf46f3ce99174ef6e6
Abdelbari <77675077+Eniyabla@users.noreply.github.com>
2022-09-21 17:05:05 +0300

    Update settings.py

--- albayine/settings.py
+++ albayine/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2$i-7%nb0k5n*$c468dfeq#wagezw8chz*^ho0&c10rt_*n89h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','abdelbari.herokuapp.com']
 
